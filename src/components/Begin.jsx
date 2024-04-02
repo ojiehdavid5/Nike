5fada4c4748431f8c23913051b9e103bf6fd72e8
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-02 17:41:25 +0100

    first commit

--- src/components/Begin.jsx
+++ src/components/Begin.jsx
@@ -7,8 +7,18 @@ const Begin = () => {
       helloworld
       
     </h1>
-    <div className="">
-        <p className='text-center'>A div</p>
+    <div className=" w-full h-10 bg-violet-200 border-violet-600 rounded-md p-2 mx-2">
+        <p className='text-center font-mono font-extrabold'>A div</p>
+    </div>
+
+
+
+
+    {/* layout */}
+
+
+    <div className='w-10 top-0 bg-red-500 fixed h-10 left-10'>
+
     </div>
 
     

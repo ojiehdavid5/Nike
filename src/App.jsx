772825dd98de73d40cd9774d732d58f97d7038d4
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-02 17:16:02 +0100

    changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Begin from './components/Begin'
+
+const App = () => {
+  return (
+<Begin/>
+  )
+}
+
+export default App
---
+++ src/components/Begin.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Begin = () => {
+  return (
+    <>
+    <h1 className='text-center text-3xl text-green-400'>
+      helloworld
+      
+    </h1>
+    <div className="">
+        <p className='text-center'>A div</p>
+    </div>
+
+    
+    </>
+      
+    
+  )
+}
+
+export default Begin

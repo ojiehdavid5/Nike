8036f3af1c117f42d31d3b1e9bad7447f616338d
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-04 16:36:07 +0100

    changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {headerLogo} from '../assets/images'
+import {hamburger} from "../assets/icons"
 
 const Nav = () => {
   return (
-    <div>
+    <header  className='padding-x py-8 z-10 w-full'>
+      <nav className='flex justify-between items-center  max-container '>
+        <a href='/'>
+          <img src=
+          
+          {headerLogo}
+          alt='Logo'
+          width={140}
+          height={29}
+          
+          ></img>
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          
+
+        </ul>
+      </nav>
+
       
-    </div>
+    </header>
   )
 }
 
